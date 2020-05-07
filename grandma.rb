faaa8ceb9e08d61b2c8538c5fee76656a2ad2e6f
Evan Bernard <epaulbernard@gmail.com>
2020-05-07 01:23:19 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,15 +13,7 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma
-  if expect(speak_to_grandma('I LOVE YOU GRANDMA!')) == 'I LOVE YOU TOO PUMPKIN!'
-    puts  "i love you too pumpkin!".upcase
-  elseif expect(speak_to_grandma('Hi Nana, how are you?')) == 'HUH?! SPEAK UP, SONNY!'
-    puts  "huh?! speak up, sonny!".upcase
-  elseif expect(speak_to_grandma('Hi!')) == 'HUH?! SPEAK UP, SONNY!'
-    puts  "huh?! speak up, sonny!".upcase
-  elseif expect(speak_to_grandma('WHAT DID YOU EAT TODAY?')) == "NO, NOT SINCE 1938!"
-    puts  "no, not since 1938!".upcase
-  elseif expect(speak_to_grandma('WHAT?')) == "NO, NOT SINCE 1938!"
-    puts  "no, not since 1938!".upcase
+  if speak_to_grandma == speak_to_grandma.upcase
+    puts "NO, NOT SINCE 1983!"
   end
 end
