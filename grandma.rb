a9bc77e2e9dcd5f476aea3c40094e4df20204895
Evan Bernard <epaulbernard@gmail.com>
2020-05-07 01:17:39 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -12,3 +12,16 @@
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
 
+def speak_to_grandma
+  if expect(speak_to_grandma('I LOVE YOU GRANDMA!')) == 'I LOVE YOU TOO PUMPKIN!'
+    puts  "i love you too pumpkin!".upcase
+  elseif expect(speak_to_grandma('Hi Nana, how are you?')) == 'HUH?! SPEAK UP, SONNY!'
+    puts  "huh?! speak up, sonny!".upcase
+  elseif expect(speak_to_grandma('Hi!')) == 'HUH?! SPEAK UP, SONNY!'
+    puts  "huh?! speak up, sonny!".upcase
+  elseif expect(speak_to_grandma('WHAT DID YOU EAT TODAY?')) == "NO, NOT SINCE 1938!"
+    puts  "no, not since 1938!".upcase
+  elseif expect(speak_to_grandma('WHAT?')) == "NO, NOT SINCE 1938!"
+    puts  "no, not since 1938!".upcase
+  end
+end
