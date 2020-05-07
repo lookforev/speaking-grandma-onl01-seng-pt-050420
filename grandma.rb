3e31b9c8b468e42bce92d9e03a1ac91a612e6eb1
Evan Bernard <epaulbernard@gmail.com>
2020-05-07 01:08:20 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,7 +13,7 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma
-  if #{speak_to_grandma} = 'I LOVE YOU GRANDMA!'
+  if speak_to_grandma = 'I LOVE YOU GRANDMA!'
     puts  "i love you too pumpkin".upcase
   elseif #{speak_to_grandma} = 'Hi Nana, how are you?'
     puts  "huh?! speak up, sonny!".upcase
