a89a5a4e2274053c05a253cd964e2caf8023281b
Evan Bernard <epaulbernard@gmail.com>
2020-05-07 00:24:50 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -11,9 +11,3 @@
 
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
-def speak_to_grandma()
-  if speak_to_grandma == speak_to_grandma.upcase
-    puts "NO, NOT SINCE 1938!"
-end
-    
-  
