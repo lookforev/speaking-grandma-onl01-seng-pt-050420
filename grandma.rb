2b2aa6f7244317beedb515e7f19f3f89b4d26380
Evan Bernard <epaulbernard@gmail.com>
2020-05-07 01:35:07 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,7 +15,7 @@
 def speak_to_grandma(phrase)
     if phrase == "I LOVE YOU GRANDMA!"
       return "I LOVE YOU TOO PUMPKIN!"
-    elsif phrase == !!phrase.upcase
+    elsif !!phrase.upcase
   		return "HUH?! SPEAK UP, SONNY!"
     elsif phrase == phrase.downcase
       return "HUH?! SPEAK UP, SONNY!"
